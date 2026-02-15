96d42eae377a0ff2e814ec8395d2181b4c743e39
vipin8488 <vipinjangra8488>
2026-02-15 17:39:30 +0530

    again trying to perform fail testing

--- app/counter.test.tsx
+++ app/counter.test.tsx
@@ -7,5 +7,6 @@ import Counter from "./counter";
 it("App Router: Works with Client Components (React State)", () => {
   render(<Counter />);
   expect(screen.getByRole("heading")).toHaveTextContent("0");
+  fireEvent.click(screen.getByRole("butto
   
 });
